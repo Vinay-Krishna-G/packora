db39b1b7e2684cf3cae6aed208419d77052f8067
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2026-05-26 12:31:29 +0530

    feat: prioritize files using the sortFiles utility before generating markdown output

--- lib/formatter/generateMarkdown.ts
+++ lib/formatter/generateMarkdown.ts
@@ -1,11 +1,12 @@
 import { ScannedFile } from "../scanner/fileTypes";
-
+import { sortFiles } from "../prioritizer/sortFiles";
 export function generateMarkdown(
     files: ScannedFile[]
 ): string {
     let markdown = "# Packora Project Context\n\n";
+    const prioritizedFiles = sortFiles(files);
 
-    for (const file of files) {
+    for (const file of prioritizedFiles) {
         markdown += `## ${file.path}\n\n`;
 
         markdown += "```" + file.extension + "\n";
